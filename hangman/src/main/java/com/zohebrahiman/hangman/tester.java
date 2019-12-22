0f2d1e097a6dbab41008b853ccfe6573999ba1c8
rahiman-zoheb <rahiman.zoheb@gmail.com>
2019-12-22 00:56:06 -0800

    Add dictionary.

--- hangman/src/main/java/com/zohebrahiman/hangman/tester.java
+++ hangman/src/main/java/com/zohebrahiman/hangman/tester.java
@@ -1,39 +1,54 @@
 package com.zohebrahiman.hangman;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class tester {
 
-	public static String[] words = new String[3];
-
-	static {
-		words[0] = "test";
-		words[1] = "random";
-		words[2] = "jazz";
-	}
+	public static List<String> words = new ArrayList<>();
+	private static File file = new File("dictionary.txt");
 
 	public static void main(String... args) {
 
+		try {
+			Scanner sc = new Scanner(file);
+			for (int i = 0; i < 100; i++) {
+				words.add(sc.next());
+			}
+			sc.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
 		Random random = new Random();
-		int wordNumber = random.nextInt(3);
+		int wordNumber = random.nextInt(words.size());
 
 		int countGuesses = 0;
 		Scanner in = new Scanner(System.in);
 
-		String wordString = words[wordNumber];
+		System.out.println("Great to have you here!!");
+
+		String wordString = words.get(wordNumber);
 		char[] guessedWordArray = new char[wordString.length()];
 		String guessedWordString = "";
 
 		while (!guessedWordString.equals(wordString)) {
-
-			// input character
+			// input a character
+			System.out.print("Please enter a single character: ");
 			String inputString = in.next();
 			if (inputString.length() > 1 || inputString.isEmpty()) {
-				System.out.println("You have to enter one character exactly!");
+				System.out.println("You have to enter exactly 1 character!");
 				continue;
 			}
 			char input = inputString.charAt(0);
+			if (input<'a' || input>'z') {
+				System.out.println("You have to enter a character!");
+				continue;
+			}
 			countGuesses++;
 
 			// if character in word, add characters at right position to guessedWord
